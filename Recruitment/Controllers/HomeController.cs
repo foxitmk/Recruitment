175fc2ee625b1dfa30ed9af0bc6fdce393d74e64
Nikola Stankovski <stankovski.n@hotmail.com>
2019-08-22 13:53:09 +0200

    asd

--- Recruitment/Controllers/HomeController.cs
+++ Recruitment/Controllers/HomeController.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Recruitment.Models;
 
 namespace Recruitment.Controllers
 {
+
     public class HomeController : Controller
     {
+        private RecruitmentEntities db = new RecruitmentEntities();
+
         public ActionResult Index()
         {
-            return View();
+            var openJobs = db.OpenJobs.Include(o => o.Questionare).Include(o => o.WorkPosition);
+            return View(openJobs.ToList());
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
